e9ba0e73a401c2c8d563337a5cd54b0bcac248b1
hzw <hzwwudi@163.com>
2020-07-23 11:47:17 +0800

    jump7

---
+++ jump7.py
@@ -0,0 +1,13 @@
+#打印1-100跳过带7和7的倍数
+
+for i in range(1,101):
+  
+    if i % 7 == 0:
+        continue
+    elif i % 10 == 7:
+        continue
+    elif i // 10 == 7:
+        continue
+    else:
+        print(i)
+
